ca0a1f41ece83e382a84ba6f5cdd27781de77eee
LENOVO <LENOVO@DESKTOP-NU1DVCB>
2025-06-12 12:52:26 -0400

    learning assi9

---
+++ src/operators2.java
@@ -0,0 +1,16 @@
+public class operators2 {
+
+    public static void main(String[] args)
+    {
+            int n=5;
+            System.out.println("pre increment valus of n is " + (++n));
+        System.out.println("After ++n valus of n is " + n);
+        System.out.println("post increment valus of n is " + (n++));
+        System.out.println("After n++ valus of n is " + n);
+        System.out.println("pre Decrement valus of n is " + (--n));
+        System.out.println("After --n valus of n is " + n);
+        System.out.println("post Decrement valus of n is " + (n--));
+        System.out.println("After n-- valus of n is " + n);
+
+    }
+}
